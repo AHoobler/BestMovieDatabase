6ee32000db95a441a37eca06397d8c4b45efd201
AHoobler <ashley.hoobler@gmail.com>
2018-03-05 15:23:03 -0500

    first commit

---
+++ BestMoviesEver/Models/Movies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BestMoviesEver.Models
+{
+    public class Movies
+    {
+        [Key]
+        public int MovieID { get; set; }
+
+        public string MovieTitle { get; set; }
+        public string MovieDescription { get; set; }
+
+        public int MovieRating { get; set; }
+    }
+}
